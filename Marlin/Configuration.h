ee584cda4b8cfd3d5b0ef24993dd395c38de2c7b
Mose <mose@fabfolk.com>
2014-04-07 19:32:37 +0200

    Changed Steps for M5 Z-Axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -388,7 +388,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // default settings
 
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,2560,1120}  // default steps per unit old
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,2560,100}  // default steps per unit
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,100}  // default steps per unit
 //#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_FEEDRATE          {400, 400, 4, 45}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
